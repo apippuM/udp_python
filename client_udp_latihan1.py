9f8ebf04f47a8b815cec62da45672b455450776b
Muhamad Afifuddin <afifudin984@gmail.com>
2024-11-20 18:53:04 +0800

    fix

--- client_udp_latihan1.py
+++ client_udp_latihan1.py
@@ -1,22 +1,21 @@
 import socket
 
-# Create a UDP socket
 soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Set a timeout for the socket
-soc.settimeout(5)  # Timeout after 5 seconds
+soc.settimeout(5)
 
 try:
-  # Input server details
+
+  # input user dan disimpan di variabel
   ip = input("Masukkan IP Server: ")
   port = int(input("Masukkan Port Server: "))
   pesan = input("Masukkan Pesan: ")
 
-  # Send message to server
+  # mengirim pesan ke server
   soc.sendto(pesan.encode('utf-8'), (ip, port))
   print(f'Pesan "{pesan}" telah dikirim ke {ip}:{port}')
 
-  # Wait for server response
+  # menunggu respon server
   try:
     data, addr = soc.recvfrom(1024)
     print(f'Pesan dari server: {data.decode("utf-8")}')
